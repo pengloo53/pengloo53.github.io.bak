df214ae0f5f86d70d1fde75fad568511e87def36
lupeng@iMac <pengloo53@gmail.com>
2022-09-01 23:00:15 +0800

    update

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -1,3 +1,34 @@
+// 切换主题
+function changeSkin(skin){
+  var _head = document.getElementsByTagName('head')[0];
+  var _link = document.createElement('link');
+  _link.setAttribute('rel', 'stylesheet')
+  _link.setAttribute('id', skin)
+  if (skin == 'dark'){
+    _link.setAttribute('href', "/assets/css/main-dark.css")
+  }else if(skin == 'default'){
+    _link.setAttribute('href', "/assets/css/main.css")
+  }
+  _head.appendChild(_link);
+  need_remove = skin=='dark'?'default':'dark';
+  if (document.getElementById(need_remove)){
+    _head.removeChild(document.getElementById(need_remove));
+  }
+}
+// 监听系统暗黑模式切换
+var themeMedia = window.matchMedia("(prefers-color-scheme: light)");
+themeMedia.addEventListener('change', e => {
+    if (e.matches) {
+      changeSkin('default');
+    } else {
+      changeSkin('dark');
+    }
+});
+// 加载页面，如果是夜间模式
+if (matchMedia('(prefers-color-scheme: dark)').matches){
+  changeSkin('dark');
+}
+
 (function () {
   var $root = document.getElementsByClassName('root')[0];
   if (window.hasEvent('touchstart')) {
@@ -5,63 +36,55 @@
     document.addEventListener('touchstart', function(){}, false);
   }
 
-  // 切换主题
-  function changeSkin(skin){
-    var _head = document.getElementsByTagName('head')[0];
-    var _link = document.createElement('link');
-    _link.setAttribute('rel', 'stylesheet')
-    _link.setAttribute('id', skin)
-    if (skin == 'dark'){
-      _link.setAttribute('href', "/assets/css/main-dark.css")
-      localStorage.setItem('skin', 'dark')
-    }else if(skin == 'default'){
-      _link.setAttribute('href', "/assets/css/main.css")
-      localStorage.setItem('skin', 'default')
-    }
-    _head.appendChild(_link);
-    need_remove = skin=='dark'?'default':'dark';
-    if (document.getElementById(need_remove)){
-      _head.removeChild(document.getElementById(need_remove));
-    }
-  }
-
-  
+ 
   // var _skin_css = document.getElementById('skin_css');
   // var _link = document.createElement('link');
   // _link.setAttribute('rel', 'stylesheet');
   // _link.setAttribute('id', 'skin_css');
 
-  var skin_ = localStorage.getItem('skin');
+  // var skin_ = localStorage.getItem('skin');
   
-  if (skin_ == 'dark'){
-    changeSkin('dark');
-  }else if(skin_ == 'defalut'){
-    changeSkin('default')
-  }
-
-  // if (matchMedia('(prefers-color-scheme: dark)').matches){
-  //     _skin_css.setAttribute('href', "/assets/css/main-dark.css");
+  // if (skin_ == 'dark'){
+  //   changeSkin('dark');
+  // }else if(skin_ == 'default'){
+  //   changeSkin('default')
   // }else{
-  //     _skin_css.setAttribute('href', "/assets/css/main.css");
+
   // }
+
+
 
   // var head = document.getElementsByTagName('head')[0];
   // head.appendChild(_link);
 
-  var _logo = document.getElementById('logo');
-  _logo.onclick = function(){
-      if (localStorage.getItem('skin') == null || localStorage.getItem('skin') == '' ||  localStorage.getItem('skin') == 'default'){
-        changeSkin('dark');
-      }else if(localStorage.getItem('skin') == 'dark'){
-        changeSkin('default');
-      }
+  // var _logo = document.getElementById('logo');
+  // _logo.onclick = function(){
+  //     if (localStorage.getItem('skin') == 'default'){
+  //       changeSkin('dark');
+  //       localStorage.setItem('skin', 'dark')
+  //     }else if(localStorage.getItem('skin') == 'dark'){
+  //       changeSkin('default');
+  //       localStorage.setItem('skin', 'default')
+  //     }else{
+  //       if
+  //     }
       
-      // if (skin == 'default' || skin == null || skin == ''){
-      //     _skin_css.setAttribute('href', '/assets/css/main-dark.css');
-      //     localStorage.setItem('skin', 'dark');
-      // }else if(skin == 'dark'){
-      //     _skin_css.setAttribute('href', '/assets/css/main.css');
-      //     localStorage.setItem('skin', 'default');
-      // }
-  }
+
+  // }
 })();
+
+
+// let lightMedia = window.matchMedia('(prefers-color-scheme: light)');
+// let darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+// let callback = (skin) => {
+//   // let prefersDarkMode = e.matches;
+//   if (skin == 'dark') {
+//     console.log('dark')
+//   }else if(skin == 'default'){
+//     console.log('light')
+//   }
+// };
+// if (typeof darkMedia.addEventListener === 'function' || typeof lightMedia.addEventListener === 'function') {
+//   lightMedia.addEventListener('change', callback('default'));
+//   darkMedia.addEventListener('change', callback('dark'));
+// }
