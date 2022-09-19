9210d5a4ad69d63ab94114f8cb3f374c136f5171
lupeng <@workpc>
2022-09-19 17:49:02 +0800

    add click function to change theme

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -1,76 +1,60 @@
 // 切换主题
-function changeSkin(skin){
+function changeSkin(skin) {
   var _head = document.getElementsByTagName('head')[0];
   var _link = document.createElement('link');
   _link.setAttribute('rel', 'stylesheet')
   _link.setAttribute('id', skin)
-  if (skin == 'dark'){
+  if (skin == 'dark') {
     _link.setAttribute('href', "/assets/css/main-dark.css")
-  }else if(skin == 'default'){
+    localStorage.setItem('skin', 'dark')
+  } else if (skin == 'default') {
     _link.setAttribute('href', "/assets/css/main.css")
+    localStorage.setItem('skin', 'default')
   }
   _head.appendChild(_link);
-  need_remove = skin=='dark'?'default':'dark';
-  if (document.getElementById(need_remove)){
+  need_remove = skin == 'dark' ? 'default' : 'dark';
+  if (document.getElementById(need_remove)) {
     _head.removeChild(document.getElementById(need_remove));
   }
 }
 // 监听系统暗黑模式切换
 var themeMedia = window.matchMedia("(prefers-color-scheme: light)");
 themeMedia.addEventListener('change', e => {
-    if (e.matches) {
-      changeSkin('default');
-    } else {
-      changeSkin('dark');
-    }
+  if (e.matches) {
+    changeSkin('default');
+  } else {
+    changeSkin('dark');
+  }
 });
-// 加载页面，如果是夜间模式
-if (matchMedia('(prefers-color-scheme: dark)').matches){
-  changeSkin('dark');
+// 首次加载页面，如果没有 localstorage 则根据系统主题；如果有 localstorage 则按照 skin 值
+if (localStorage.getItem('skin') == 'undefined' || localStorage.getItem('skin') == '' || localStorage.getItem('skin') == null) {
+  if (matchMedia('(prefers-color-scheme: dark)').matches) {
+    changeSkin('dark');
+  }
+} else {
+  if (localStorage.getItem('skin') == 'dark') {
+    changeSkin('dark');
+  } else if (localStorage.getItem('skin') == 'default') {
+    changeSkin('default');
+  }
 }
 
 (function () {
   var $root = document.getElementsByClassName('root')[0];
   if (window.hasEvent('touchstart')) {
     $root.dataset.isTouch = true;
-    document.addEventListener('touchstart', function(){}, false);
+    document.addEventListener('touchstart', function () { }, false);
   }
 
- 
-  // var _skin_css = document.getElementById('skin_css');
-  // var _link = document.createElement('link');
-  // _link.setAttribute('rel', 'stylesheet');
-  // _link.setAttribute('id', 'skin_css');
-
-  // var skin_ = localStorage.getItem('skin');
-  
-  // if (skin_ == 'dark'){
-  //   changeSkin('dark');
-  // }else if(skin_ == 'default'){
-  //   changeSkin('default')
-  // }else{
-
-  // }
-
-
-
-  // var head = document.getElementsByTagName('head')[0];
-  // head.appendChild(_link);
-
-  // var _logo = document.getElementById('logo');
-  // _logo.onclick = function(){
-  //     if (localStorage.getItem('skin') == 'default'){
-  //       changeSkin('dark');
-  //       localStorage.setItem('skin', 'dark')
-  //     }else if(localStorage.getItem('skin') == 'dark'){
-  //       changeSkin('default');
-  //       localStorage.setItem('skin', 'default')
-  //     }else{
-  //       if
-  //     }
-      
-
-  // }
+  // 点击 logo 切换
+  var _logo = document.getElementById('logo');
+  _logo.onclick = function () {
+    if (localStorage.getItem('skin') == 'dark') {
+      changeSkin('default');
+    } else {
+      changeSkin('dark');
+    }
+  }
 })();
 
 
